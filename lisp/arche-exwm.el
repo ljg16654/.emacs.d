f34e1b0d470c7496e75f31fdb73a6cf8bcefabf5
Jigang Li <ljg16654@sjtu.edu.cn>
2021-06-05 16:10:07 +0800

    improve exwm config

--- lisp/arche-exwm.el
+++ lisp/arche-exwm.el
@@ -1,4 +1,7 @@
 (require 'arche-package)
+
+(defvar arche/exwm-enabled nil)
+
 (use-package exwm
   :init
   (setq mouse-autoselect-window nil
@@ -10,40 +13,55 @@
   (add-hook 'exwm-update-class-hook
             (lambda ()
               (exwm-workspace-rename-buffer exwm-class-name)))
+  
+  (require 'arche-desktop)
   (add-hook 'exwm-update-title-hook
             (lambda ()
               (pcase exwm-class-name
-                ("qutebrowser" (exwm-workspace-rename-buffer (format "Qutebrowser: %s" exwm-title))))))
+                ("qutebrowser" (exwm-workspace-rename-buffer (format "Qutebrowser: %s" exwm-title)))
+		("Alacritty" (exwm-workspace-rename-buffer (format "Alacritty: %s" exwm-title))))))
   
+  (add-hook 'exwm-init-hook #'(lambda () (setq arche/exwm-enabled t)))
+
+  
+  (define-key exwm-mode-map (kbd "C-q") #'exwm-input-send-next-key)
+  (define-key exwm-mode-map (kbd "χ") #'other-window)
+  (define-key exwm-mode-map (kbd "ρ") #'(lambda () (interactive)
+					  (other-window -1)))
+  (global-set-key (kbd "θ") #'ace-window)
+  (define-key exwm-mode-map (kbd "s-o") #'switch-to-buffer)
+  
+  (define-key exwm-mode-map (kbd "s-p") #'previous-buffer)
+  (define-key exwm-mode-map (kbd "s-n") #'next-buffer)
+  (define-key exwm-mode-map (kbd "s-k") #'(lambda () (interactive)
+					    (kill-buffer (current-buffer))))
+
   (setq exwm-input-global-keys
 	;; only for exwm-related commands
 	(list
 	 (cons (kbd "s-r") #'exwm-reset)
-	 (cons (kbd "s-g") #'exwm-workspace-switch)))
-
+	 (cons (kbd "s-g") #'exwm-workspace-switch)
+	 (cons (kbd "s-t") #'arche/toggle-touchpad)
+	 (cons (kbd "s-q") #'(lambda () (interactive) (arche/raise-or-run "qutebrowser" "Qutebrowser: ")))
+	 (cons (kbd "s-a") #'(lambda () (interactive) (arche/raise-or-run "alacritty" "Alacritty: ")))))
+  
   ;; toggle touchpad
   (defun arche/toggle-touchpad ()
     (interactive)
     "Toggle the dumb touchpad."
     (start-process-shell-command "sh" nil " sh ~/scripts/toggleTouchpad.sh"))
+
+  ;; init tmux sessions
+  (efs/run-in-background "sh ~/scripts/tmux_initialize_sessions.sh")
   
-  ;; TODO write macro to do it together with global-set-key
-  (define-key exwm-mode-map (kbd "C-q") #'exwm-input-send-next-key)
-  (define-key exwm-mode-map (kbd "s-o") #'consult-buffer)
-  (define-key exwm-mode-map (kbd "χ") #'other-window)
-  (define-key exwm-mode-map (kbd "s-t") #'arche/toggle-touchpad)
-  (global-set-key (kbd "s-t") #'arche/toggle-touchpad)
-
-  ;; TODO implement run-or-raise in stumpwm
-
-  ;; TODO find a decent launcher
-
-  ;; TODO bufler setup
-
-  ;; remap before sending to the X window 
+  ;; remap before sending to the X window
   (setq exwm-input-simulation-keys
+	;; Use (read-key) to get cdr of the cons list
 	(list
-	 (cons (kbd "M-w") (kbd "C-c"))))
+	 ;; M-w |-> C-c
+	 (cons (kbd "M-w") [3])
+	 ;; C-y |-> C-v
+	 (cons (kbd "C-y") [22])))
   
   (exwm-enable)
   (require 'exwm-randr)
