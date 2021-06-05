34fd8876b031c0c845ac30fc06662c224a2739f2
Jigang Li <ljg16654@sjtu.edu.cn>
2021-06-05 16:09:12 +0800

    simulate run-or-raise in stumpwm, desktop-environment

---
+++ lisp/arche-desktop.el
@@ -0,0 +1,27 @@
+(require 'arche-package)
+
+(use-package desktop-environment
+  :after exwm
+  :config (desktop-environment-mode))
+
+(defun efs/run-in-background (command)
+  (let ((command-parts (split-string command "[ ]+")))
+    (apply #'call-process `(,(car command-parts) nil 0 nil ,@(cdr command-parts)))))
+
+;; stumpwm simulation
+(defun arche-desktop/filter-buffer-by-prefix (prefix)
+  (-filter
+   (lambda (buf)
+     (s-prefix-p prefix (buffer-name buf)))
+   (buffer-list)))
+
+(defun arche/raise-or-run (cmd prefix)
+  (let
+      ((buf (car (arche-desktop/filter-buffer-by-prefix prefix))))
+    (if buf
+	(if (get-buffer-window buf)
+	    (select-window (get-buffer-window buf))
+	  (switch-to-buffer buf))
+      (efs/run-in-background cmd))))
+
+(provide 'arche-desktop)
