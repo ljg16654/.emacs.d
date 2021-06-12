46fed5c92abb6f7f19750f1d124a0a4fc2df6876
Jigang Li <ljg16654@sjtu.edu.cn>
2021-06-13 00:45:03 +0800

    [Mail] mu4e revives

--- lisp/arche-email.el
+++ lisp/arche-email.el
@@ -16,36 +16,33 @@
 ;;* notmuch
 (require 'notmuch)
 
-;;* mu4e
-;; (add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e")
-;; (require 'org-mu4e)
-;; (require 'mu4e)
+(use-package mu4e
+  :load-path "~/src/mu/mu4e"
+  :init
+  (require 'org-mu4e)
+  (require 'mu4e)
+  :config
+  (setq mail-user-agent 'mu4e-user-agent)
+  (setq mu4e-maildir "/home/jigang/Maildir")
+  (setq mu4e-get-mail-command "offlineimap")
+  (setq message-send-mail-function 'smtpmail-send-it)
+  (setq mu4e-contexts
+	`( ,(make-mu4e-context
+	     :name "UMich"
+	     :match-func (lambda (msg) (when msg
+				    (string-prefix-p "/UMich" (mu4e-message-field msg :maildir))))
+	     :vars '(
+		     (mu4e-trash-folder . "/UMich/[Gmail].Trash")
+		     (mu4e-refile-folder . "/UMich/[Gmail].Archive")
+		     (smtpmail-smtp-server . "smtp.sjtu.edu.cn")
+		     ))
+	   ,(make-mu4e-context
+	     :name "SJTU"
+	     :match-func (lambda (msg) (when msg
+				    (string-prefix-p "/SJTU" (mu4e-message-field msg :maildir))))
+	     :vars '( 		   
+		     (mu4e-trash-folder . "/SJTU/Trash")
+		     (smtpmail-smtp-server . "smtp.gmail.com"))))))
 
-;; (use-package mu4e
-;;   :load-path "/usr/share/emacs/site-lisp/mu4e")
-
-;; (setq mail-user-agent 'mu4e-user-agent)
-;; (setq mu4e-maildir "/home/jigang/Maildir")
-;; (setq mu4e-get-mail-command "offlineimap")
-;; (setq message-send-mail-function 'smtpmail-send-it)
-;; (setq mu4e-contexts
-;;       `( ,(make-mu4e-context
-;; 	   :name "UMich"
-;; 	   :match-func (lambda (msg) (when msg
-;; 				       (string-prefix-p "/UMich" (mu4e-message-field msg :maildir))))
-;; 	   :vars '(
-;; 		   (mu4e-trash-folder . "/UMich/[Gmail].Trash")
-;; 		   (mu4e-refile-folder . "/UMich/[Gmail].Archive")
-;; 		   (smtpmail-smtp-server . "smtp.sjtu.edu.cn")
-;; 		   ))
-;; 	 ,(make-mu4e-context
-;; 	   :name "SJTU"
-;; 	   :match-func (lambda (msg) (when msg
-;; 				       (string-prefix-p "/SJTU" (mu4e-message-field msg :maildir))))
-;; 	   :vars '( 		   
-;; 		   (mu4e-trash-folder . "/SJTU/Trash")
-;; 		   (smtpmail-smtp-server . "smtp.gmail.com")
-;; 		   )))
-;;       )
 
 (provide 'arche-email)
