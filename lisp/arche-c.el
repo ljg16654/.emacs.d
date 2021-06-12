b0a2d1a01dad839fc7b7f78994462d0723ab19da
Jigang Li <ljg16654@sjtu.edu.cn>
2021-06-13 00:44:31 +0800

    [C++] c++-mode hooks

--- lisp/arche-c.el
+++ lisp/arche-c.el
@@ -7,6 +7,11 @@
 
 (add-hook 'c-mode-hook #'c-mode-my-basic-settings)
 (add-hook 'c++-mode-hook #'c-mode-my-basic-settings)
+(define-key c++-mode-map (kbd "C-k") #'crux-smart-kill-line)
+(define-key c++-mode-map (kbd "C-k") #'crux-smart-kill-line)
+
+(add-hook 'c++-mode-hook
+	  (lambda () (add-hook 'after-save-hook #'clang-format-buffer)))
 
 (use-package clang-format)
 (provide 'arche-c)
