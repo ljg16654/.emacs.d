3ac1504a34a11bdc74b8a4e7e8666e6264c35a64
Jigang Li <ljg16654@sjtu.edu.cn>
2021-06-18 19:11:22 +0800

    [Misc] dos2unix

--- lisp/arche-misc.el
+++ lisp/arche-misc.el
@@ -171,4 +171,15 @@ natural for reading."
   :config
   (setq keyfreq-excluded-commands '()))
 
+(defun dos2unix (buffer)
+  "Automate M-% C-q C-m RET C-q C-j RET"
+  (interactive "*b")
+  (save-excursion
+    (goto-char (point-min))
+    (while (search-forward (string ?\C-m) nil t)
+      (replace-match (string ?\C-j) nil t))))
+
+(global-set-key (kbd "s-y") #'consult-register)
+
 (provide 'arche-misc)
+
